b50a0e8ab0c18ba8b337c82b97a17b040a949c39
umpulumpa <twan.lensen@hotmail.com>
2023-07-08 03:36:54 +0200

    dawd

--- commands/getcode.js
+++ commands/getcode.js
@@ -1,12 +1,36 @@
 const {
     SlashCommandBuilder,
 } = require('discord.js');
+const { checkAdmin } = require('../functions/checkAdmin');
+const { checkAllowed } = require('../functions/checkAllowed');
 const { tryReadFile } = require('../functions/tryReadFile');
 const { trySetFile } = require('../functions/trySetFile');
 
-const filePath = "./assets/data/codes.json"
+async function trygetCode(client, interaction) {
+    if (!(await checkAdmin(client, interaction)) && !checkAllowed(interaction)) {
+        let limitFile = tryReadFile("userLimits.json", interaction.guild.id)
+        let userObj = limitFile.findIndex(item => item.userId === interaction.user.id)
+        if (limitFile[userObj] != undefined && userObj > -1) {
+            if (limitFile[userObj].currentCount != "*" && limitFile[userObj].limit >= limitFile[userObj].currentCount) {
+              return false
+            } else {
+              limitFile[userObj].currentCount += 1
+              limitFile[userObj].lastUpdate = Date.now()
+              return true
+            }
+          } else {
+            const userLimits =  {
+                "userId": interaction.user.id,
+                "limit": 1,
+                "currentCount": 1,
+                "lastUpdate": Date.now()
+            }
+            limitFile.push(userLimits)
+        }
+        trySetFile(`./assets/data/servers/${interaction.guild.id}/userLimits.json`, limitFile)
+    }
 
-function trygetCode(interaction) {
+    
     let codeFile = tryReadFile("codes.json", interaction.guild.id)
     if (codeFile === false) {
         return "There was an error trying to get the codes"
@@ -28,6 +52,6 @@ module.exports = {
         .setName('getcode')
         .setDescription('Gets a clan invite code.'),
     async execute(client, interaction) {
-        return { content: trygetCode(interaction), ephemeral: true }
+        return { content: await trygetCode(client, interaction), ephemeral: true }
     },
 }
--- twitch-bot/app.js
+++ twitch-bot/app.js
@@ -1,17 +1,22 @@
 const tmi = require('tmi.js');
 require('dotenv').config();
 const fs = require('fs');
-const { trySetFile } = require('../functions/trySetFile');
+const {
+  trySetFile
+} = require('../functions/trySetFile');
 
 const paths = {
   "serverConnections": "../assets/data/serverConnections.json",
   "codes": "../assets/data/servers/{{serverId}}/codes.json",
   "logs": "../assets/data/servers/{{serverId}}/logs.json",
+  "userLimits": "../assets/data/servers/{{serverId}}/userLimits.json",
 }
 
 
 const client = new tmi.Client({
-  options: { debug: true },
+  options: {
+    debug: true
+  },
   connection: {
     secure: true,
     reconnect: true
@@ -27,40 +32,73 @@ const client = new tmi.Client({
 function getFile(path) {
   const jsonData = fs.readFileSync(path, 'utf8');
   try {
-      const data = JSON.parse(jsonData);
-      return data
+    const data = JSON.parse(jsonData);
+    return data
   } catch (error) {
-      console.log(error)
-      return false
+    console.log(error)
+    return false
   }
 }
 
 function logCommand(serverId, channel, tags) {
   let logs = getFile(paths["logs"].replace("{{serverId}}", serverId))
   if (logs !== false) {
-      const log = {
-          "timestamp": Date.now(),
-          "userId": tags["user-id"],
-          "userName": tags.username,
-          "channelId": "twitch",
-          "channelName": channel,
-          "command": "getCode",
+    const log = {
+      "timestamp": Date.now(),
+      "userId": tags["user-id"],
+      "userName": tags.username,
+      "channelId": "twitch",
+      "channelName": channel,
+      "command": "getCode",
+    }
+    logs.push(log)
+    trySetFile(paths["logs"].replace("{{serverId}}", serverId), logs)
+  }
+}
+
+function checkLimit(discordServerId, tags) {
+  const limitPath = paths["userLimits"].replace("{{serverId}}", discordServerId)
+  if (fs.existsSync(limitPath)) {
+    let limitFile = getFile(limitPath)
+    let userObj = limitFile.findIndex(item => item.userId === tags["user-id"])
+    if (limitFile[userObj] != undefined && userObj > -1) {
+      if (limitFile[userObj].currentCount != "*" && limitFile[userObj].limit >= limitFile[userObj].currentCount) {
+        return false
+      } else {
+        limitFile[userObj].currentCount += 1
+        limitFile[userObj].lastUpdate = Date.now()
+        return true
       }
-      logs.push(log)
-      trySetFile(paths["logs"].replace("{{serverId}}", serverId), logs)
+    } else {
+      const userLimits = {
+        "userId": tags["user-id"],
+        "limit": 1,
+        "currentCount": 1,
+        "lastUpdate": Date.now()
+      }
+      limitFile.push(userLimits)
+    }
+    trySetFile(limitPath, limitFile)
+    return true
+  } else {
+    return false
   }
 }
 
 function getCode(channel, tags) {
   const channels = getFile(paths["serverConnections"])
   const channelConnection = channels.find(x => x.twitchName === channel)
+  if (channelConnection == undefined) return "Couldn't find database."
   const discordServerId = channelConnection.discordServerId
-  const codesPath =paths["codes"].replace("{{serverId}}", discordServerId)
+  const codesPath = paths["codes"].replace("{{serverId}}", discordServerId)
   if (fs.existsSync(codesPath)) {
-    let codeFile = getFile(paths["codes"].replace("{{serverId}}", discordServerId))
+    // console.log(checkLimit(discordServerId, tags))
+    // return "x"
+    let codeFile = getFile(codesPath)
     if (codeFile !== false && codeFile.length > 0) {
+      if (!checkLimit(discordServerId, tags)) return "You've already claimed all allowed codes. Please contact an admin if you need help."
       const code = codeFile.shift();
-      trySetFile(paths["codes"].replace("{{serverId}}", discordServerId), codeFile)
+      trySetFile(codesPath, codeFile)
       logCommand(discordServerId, channel, tags)
       return `${code.code}`
     } else {
@@ -76,8 +114,8 @@ client.connect();
 
 client.on('message', (channel, tags, message, self) => {
   // Ignore echoed messages.
-  if(self) return;
-  if(message.toLowerCase().trim().startsWith('!code')) {
+  if (self) return;
+  if (message.toLowerCase().trim().startsWith('!code')) {
     client.say(channel, `@${tags.username}, ${getCode(channel, tags)}`);
   }
 });
